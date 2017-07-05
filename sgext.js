bdb202773ef6b9c985bb3268df3813357038bc31
Brian C Sparks <brian.c.sparks@hp.com>
2017-07-05 11:58:53 -0700

    added fs-extra, sg.fs now available

--- sgext.js
+++ sgext.js
@@ -7,16 +7,19 @@
 
 exports.load = function(sg, _) {
 
-  var async           = sg.extlibs.async        = require('async');
   var mkdirp          = sg.extlibs.mkdirp       = require('mkdirp');
   var request         = sg.extlibs.superagent   = require('superagent');
   var moment          = sg.extlibs.moment       = require('moment');
   var shelljs         = sg.extlibs.shelljs      = require('shelljs');
+  var fsExtra         = sg.extlibs.fsExtra      = require('fs-extra');
   var split           = sg.extlibs.split        = require('split');
   var chalk           = sg.extlibs.chalk        = require('chalk');
   var mime            = sg.extlibs.mime         = require('mime-types');
 
   sg.extlibs.request  = sg.extlibs.superagent;
+  sg.extlibs.fs       = sg.extlibs.fsExtra;
+
+  sg.fs = sg.extend(sg.extlibs.fs, sg.extlibs.shelljs);
 
   var path            = require('path');
 

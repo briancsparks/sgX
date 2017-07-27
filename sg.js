e9c20709000119b9143dc35b151324d7a07637c9
Brian C Sparks <brian.c.sparks@hp.com>
2017-07-27 08:49:53 +0000

    Fixed bug in parseStringOn2Chars

--- sg.js
+++ sg.js
@@ -427,7 +427,7 @@ sg.startsWith = function(longStr, start) {
 sg.parseOn2Chars = function(str, sep1, sep2) {
   var ret = {};
   _.each(str.split(sep1), function(kv) {
-    var arr = kv.split(sep2), k = arr[0], v = arr[1] || true;
+    var arr = kv.split(sep2), k = arr[0], v = arr[1] || '';
     if (k && v) {
       ret[k] = v;
     }
